b1a1173e49bb4274fc0e7a78fc199e72569bdca6
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-06-25 14:42:55 -0700

    Solution to exercise 01 through 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,9 @@
 const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
     let result = '';
     for (let i = 0; i < num; i++) {
         result += string;
-    } return string;
+    } return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = ''
+    while (string.length > 0) {
+        result += string[string.length - 1]
+        string = string.slice(0,-1)
+    } return result;
 };
 
 // Do not edit below this line
