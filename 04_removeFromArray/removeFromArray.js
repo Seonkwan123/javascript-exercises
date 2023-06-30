2031819028daaef3784f9345745ab33d280758b4
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-06-30 05:42:35 -0700

    Provide solution from 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+// This one is my first try. Will do another one with filter function
 
-};
+// const removeFromArray = function(numArray, ...numDelete) {
+//     const result = [];
+//     numDelete = Array.from(numDelete);
+//     for (const num of numArray) {
+//         let status = 0;
+//         for (let i = 0; i < numDelete.length; i++) {
+//             if (num !== numDelete[i]) {
+//                 status = 1;
+//             } else if (num === numDelete[i]) {
+//                 status = 0;
+//                 break;
+//             }
+//         }
+//         console.log(result)
+//         if (status) result.push(num);
+//     }
+//     return result;
+// };
+
+const removeFromArray = function(numArray, ...numDelete) {
+ return numArray.filter(num => !numDelete.includes(num)) 
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  let fahrenheit = ((temp1-32)*5)/9;
+  if (fahrenheit % 1 !== 0) {
+    return Number(fahrenheit.toFixed(1))
+  }
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let celcius = ((temp2*(9/5)) + 32);
+  if (celcius % 1 !== 0) {
+    return Number(celcius.toFixed(1));
+  }
+  return celcius;
 };
 
 // Do not edit below this line
